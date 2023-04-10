fedb628e243222e065da292db5a0a6ed16759bbd
Benjamin Mellon <mellonbenjamin@gmail.com>
2023-04-10 13:47:10 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MariageHome from './components/MariageHome.vue'
+</script>
+
+<template>
+	<head>
+		<title>Mariage</title>
+	</head>
+  <div>
+  </div>
+  <MariageHome />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/MariageHome.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+	<div class="p-5 bg-white rounded">
+		<h1 >
+			Mariage de Morgane et Romain <br>
+			11 novembre 2023 <br>
+			Castel Perralda, Espagne
+		</h1>
+		<img src="/src/assets/vista-de-la-fachada-principal.jpg" class="fade-in">
+	</div>
+	
+</template>
+
+<style scoped>
+
+</style>
